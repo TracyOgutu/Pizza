837818f83b45403942502bb9820f41d07e2d1e56
Tracy Ogutu <tracyogutu@gmail.com>
2019-10-06 19:24:01 +0300

    refactor the address code in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,130 +1,116 @@
-// function Order(size, crusts, toppings){
-//     this.size=size;
-//     this.crusts=crusts;
-//     this.toppings=toppings;
-
-// }
-
-
 $(document).ready(function(){
+    function Order(size, toppings, crust, quantity, type){
+        this.Size = size;
+        this.Toppings = toppings;
+        this.Crust = crust;
+        this.Quantity = quantity;
+        this.Type = type;
+    }
+    // Order.prototype.Address = function(city,estate,street,housenumber){
+    //     this.City = city;
+    //     this.Estate = estate;
+    //     this.Street = street;
+    //     this.Housenumber = housenumber;
+    // }
 
     $("input#yes").click(function(){
-
         $(".address").show();
-        $(".address-summary").show(); //bug 1: address summary showing immediately 
+        
         alert("Delivery charge: 200 Ksh");
         alert("Your order will be delivered to your location");
-
-
     });
 
     $("input#no").click(function(){
         
         $(".address").hide();
         $(".address-summary").hide();
-        $(".delivery-fee").hide();
-
-
     });
 
-    function calculatePizza(size,quantity,crusts,toppings,delivery){
+    function calculatePizza(sizePrice,quantity,crustPrice,toppingsPrice,test)
+    {
 
-        var total= size * quantity;
-        var result =(total + crusts + toppings+ delivery);
+        var total= sizePrice * quantity;
+
+        var result =(total + crustPrice + toppingsPrice+ test);
 
         
         return result;    
     
     }
 
+    function deliveryFee()
+    {
+        var deli=$('input[name=delivery]:checked').val();
+        var delivery;
+        if (deli=="1")
+        {
+            delivery = 200;
+            $(".address-summary").show();
+
+            return delivery;
+        }
+        else
+        {
+            delivery = 0;
+            return delivery;
+        }
+
+    }
+
     
 
-   $("form#pizza-form").submit(function(event){
+   $("form#pizza-form").submit(function(event)
+   {
             event.preventDefault();
 
-            var type=$(".type").val();   //bug 4: Type of pizza not displaying correctly in summary
-            var size=parseInt($(".size").val());
-            var crusts=parseInt($(".crusts").val());
-            var toppings=parseInt($(".toppings").val());
-            var quantity=parseInt($("input#quantity").val());
+            var type=$('input[name=type]:checked').val();  
 
-            var delivery=parseInt(200); //bug 3:  delivery fee not included in calculation
-                                        //    3.1 delivery fee persists even when no delivery is required
+            var size =$(".size").val();
+            var sizeSplit = size.split(", ");
+            var sizePrice = parseInt(sizeSplit[1]);
+            
 
-            var total= size * quantity;
-            var result =(total + crusts + toppings);
+            var crusts=$(".crusts").val();
+            var crustSplit = crusts.split(", ");
+            var crustPrice=parseInt(crustSplit[1]);
+
+
+            var toppings=$(".toppings").val();
+            var toppingsSplit=toppings.split(", ");
+            var toppingsPrice=parseInt(toppingsSplit[1]);
+
+            var quantity=parseInt($("input#quantity").val());                         
+
+            var test = deliveryFee();    
 
             var city=$(".city").val();
             var estate=$(".estate").val();
             var street=$(".street").val();
             var housenumber=parseInt($(".housenumber").val());
 
-            calculatePizza(size,quantity,crusts,toppings);
+            var orderamount = calculatePizza(sizePrice,quantity,crustPrice,toppingsPrice,test);
 
-            $(".show-order").show(); //bug 2:Order summary does not show names of order items
+            var newOrder = new Order(sizeSplit, toppingsSplit, crustSplit, quantity, type);
+            console.log(newOrder);
 
+            // var newAddress = new newOrder.Address(city,estate,street,housenumber);
 
-            $(".pizza-type").text(type);
-            $(".order-size").text(size);
-            $(".order-quantity").text(quantity);
-            $(".order-crusts").text(crusts);
-            $(".order-toppings").text(toppings);
-            $(".delivery-fee").text(delivery);
-            $(".order-total").text(result);
+            $(".show-order").show(); 
+
+            $(".pizza-types").text(type);
+            $(".order-size").text(newOrder.Size[0] + " Ksh: " + newOrder.Size[1]);
+            $(".order-quantity").text(newOrder.Quantity);
+            $(".order-crusts").text(newOrder.Crust[0] + " Ksh: " + newOrder.Crust[1]);
+            $(".order-toppings").text(newOrder.Toppings[0] + " Ksh: " + newOrder.Toppings[1]);
+            $(".delivery-fee").text(test);
+            $(".order-total").text(orderamount);
 
             $(".address-city").text(city);
             $(".address-estate").text(estate);
             $(".address-street").text(street);
-            $(".address-housenumber").text(housenumber);
-
-           
-            
-            
-
-            // var smallpizza=parseInt($("input#small").val());
-            // var mediumpizza=parseInt($("input#medium").val());
-            // var largepizza=parseInt($("input#large").val());
-
-            // var crispycrust=parseInt($("input#crispy").val());
-            // var stuffedcrust=parseInt($("input#stuffed").val());
-            // var glutenfreecrust=parseInt($("input#glutenfree").val());
-
-            // var meattoppings=parseInt($("input#meat").val());
-            // var vegetabletoppings=parseInt($("input#vegetable").val());
-            // var mixedtoppings=parseInt($("input#mixed").val());
-
-        
-            
-
-            // var yes=$("input#yes");
-            // alert(yes);
-            
+            $(".address-housenumber").text(housenumber);         
             
   });
 
-  
-
 });
-// function calculate(){
-
-// var size=document.querySelector('input[name="size"]:checked').value;
-
-// if(size==1){
-//     alert("small");
-//     var small=300;
-// }
-
-// var crust=document.querySelector('input[name="crust"]:checked').value;
-// if(crust==1){
-//     alert("crispy");
-//     var crispy = 200;
-// }
-
-// var toppings=document.querySelector('input[name="toppings"]:checked').value;
-// if(toppings==1){
-//     alert("meat");
-//     var meat=200;
-// }
-
-// }
 
